2c11cfb4116e9e3fe8188d86cd8d902a7fe76b32
Kapatidpo12 <69280695+Kapatidpo12@users.noreply.github.com>
2026-01-28 00:09:09 +0000

    Added basic CRUD operations for Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,54 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create 
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to books_path
+    else
+      flash[:notice] = "Book could not be created. Please check required fields"
+      render('new')
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to books_path
+    else
+      flash[:notice] = "Book could not be updated. Please check required fields"
+      render('edit')
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed"
+    redirect_to books_path
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :price, :published_date)
+  end
 end
