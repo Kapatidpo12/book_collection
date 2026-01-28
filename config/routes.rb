9b006550df387fdd360e49b6e0e90efc00f07442
Kapatidpo12 <69280695+Kapatidpo12@users.noreply.github.com>
2026-01-28 00:11:51 +0000

    Added resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+  
+  root 'books#index'
+
 end
